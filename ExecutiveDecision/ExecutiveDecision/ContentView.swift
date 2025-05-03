0cd15990f4f04c1a5813cc3ce9b2801caa23cf70
AD <silentflute.ad@gmail.com>
2025-05-03 09:11:02 +0100

    Initial project build

---
+++ ExecutiveDecision/ExecutiveDecision/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  ExecutiveDecision
+//
+//  Created by Amarjit on 03/05/2025.
+//
+
+import SwiftUI
+struct ContentView: View {
+
+    var body: some View {
+        Text("Executive Decision")
+    }
+}
+
+#Preview {
+    ContentView()
+}
